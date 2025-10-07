5358372bf5ec07b38863cb25e05182beb2779798
uminocelo <marcelo.mdl.dev@gmail.com>
2025-10-06 22:37:17 -0300

    feat: add mdex dependency to project

--- mix.exs
+++ mix.exs
@@ -62,7 +62,8 @@ defmodule Manto.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:mdex, "~> 0.8"}
     ]
   end
 
